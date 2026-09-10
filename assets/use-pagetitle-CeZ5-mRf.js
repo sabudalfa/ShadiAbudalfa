c50610467a7c3947447d5ae0bb6e38240a7dfec3
sabudalfa <sabudalfa@users.noreply.github.com>
2026-09-10 05:57:28 +0000

    [ci] deploy site 553decae13962bf64f47ab9ca9e71a5d683c39d5

--- assets/use-pagetitle-CeZ5-mRf.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-BR7caCVG.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
---
+++ assets/use-pagetitle-CwTZVtH1.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-amwmv3sY.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
